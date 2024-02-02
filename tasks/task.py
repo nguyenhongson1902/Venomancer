a44f9f0e7e27a313bd968af3bb5e008d3ba555a0
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2024-02-02 14:13:57 +0700

    Add MNISTConditionalAutoencoder to Task class

--- tasks/task.py
+++ tasks/task.py
@@ -421,7 +421,7 @@ class Task:
     
     def get_atkmodel(self):
         if self.params.task.lower() == 'cifar10':
-            from attack_models.autoencoders import ConditionalAutoencoder, Autoencoder
+            from attack_models.autoencoders import ConditionalAutoencoder, Autoencoder, MNISTConditionalAutoencoder
             from attack_models.unet import UNet
 
             input_dim = self.params.input_shape[1]
@@ -435,13 +435,41 @@ class Task:
                         ])
             
             # atkmodel = ConditionalAutoencoder(n_classes, input_dim, pattern_tensor).to(self.params.device)
-            atkmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            # atkmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            atkmodel = MNISTConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
             # atkmodel = UNet(n_classes, input_dim, 3).to(self.params.device)
             # atkmodel = Autoencoder().to(self.params.device)
             # atkmodel = UNet(3).to(self.params.device)
 
             # tgtmodel = ConditionalAutoencoder(n_classes, input_dim, pattern_tensor).to(self.params.device)
-            tgtmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            # tgtmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            tgtmodel = MNISTConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            # tgtmodel = UNet(n_classes, input_dim, 3).to(self.params.device)
+            # tgtmodel = Autoencoder().to(self.params.device)
+            # tgtmodel = UNet(3).to(self.params.device)
+            tgtmodel.load_state_dict(atkmodel.state_dict(), strict=True)
+
+            # tgtoptimizer = torch.optim.Adam(tgtmodel.parameters(), lr=self.params.lr_atk)
+            tgtoptimizer = torch.optim.Adam(tgtmodel.parameters(), lr=self.params.lr_atk, betas=(0.5, 0.999)) # Starts from exp67
+
+            return atkmodel, tgtmodel, tgtoptimizer
+        elif self.params.task.lower() == 'mnist':
+            from attack_models.autoencoders import MNISTConditionalAutoencoder
+            from attack_models.unet import UNet
+
+            input_dim = self.params.input_shape[1]
+            n_classes = self.params.num_classes
+            
+            # atkmodel = ConditionalAutoencoder(n_classes, input_dim, pattern_tensor).to(self.params.device)
+            # atkmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            atkmodel = MNISTConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            # atkmodel = UNet(n_classes, input_dim, 3).to(self.params.device)
+            # atkmodel = Autoencoder().to(self.params.device)
+            # atkmodel = UNet(3).to(self.params.device)
+
+            # tgtmodel = ConditionalAutoencoder(n_classes, input_dim, pattern_tensor).to(self.params.device)
+            # tgtmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            tgtmodel = MNISTConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
             # tgtmodel = UNet(n_classes, input_dim, 3).to(self.params.device)
             # tgtmodel = Autoencoder().to(self.params.device)
             # tgtmodel = UNet(3).to(self.params.device)
