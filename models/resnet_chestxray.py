f6ce6921548ac115c2cb31ccca0988903024e1b6
Son Nguyen <nguyenhongsonk62hust@gmail.com>
2024-02-04 22:19:53 +0700

    Update ResNet architecture in resnet_chestxray.py

--- models/resnet_chestxray.py
+++ models/resnet_chestxray.py
@@ -69,8 +69,8 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        # self.conv0 = nn.Conv2d(1, 3, kernel_size=5, stride=1, padding=2, bias=False) # (1, 1024, 1024) -> (3, 1024, 1024)
-        # self.bn0 = nn.BatchNorm2d(3)
+        self.conv0 = nn.Conv2d(1, 3, kernel_size=5, stride=1, padding=2, bias=False) # (1, 1024, 1024) -> (3, 1024, 1024)
+        self.bn0 = nn.BatchNorm2d(3)
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -78,7 +78,10 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        # self.linear = nn.Linear(25088, num_classes)
+        # self.linear = nn.Linear(524288, num_classes)
+        self.linear = nn.Linear(32768, num_classes)
+        # self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -89,9 +92,9 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        # out = F.relu(self.bn0(self.conv0(x))) # (1, 1024, 1024) -> (3, 1024, 1024)
-        out = F.relu(self.bn1(self.conv1(x)))
-        # out = F.relu(self.bn1(self.conv1(out))) # to be compatible with (1, 1024, 1024) -> (3, 1024, 1024)
+        out = F.relu(self.bn0(self.conv0(x))) # (1, 1024, 1024) -> (3, 1024, 1024)
+        # out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn1(self.conv1(out))) # to be compatible with (1, 1024, 1024) -> (3, 1024, 1024)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
