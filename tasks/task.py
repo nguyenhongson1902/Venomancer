e5623e5083b1e8bb8fecad7d07163bb049089c3e
Son Nguyen <nguyenhongsonk62hust@gmail.com>
2024-02-09 23:01:15 +0700

    Update attack and target models in Task class

--- tasks/task.py
+++ tasks/task.py
@@ -528,22 +528,24 @@ class Task:
             return atkmodel, tgtmodel, tgtoptimizer
         elif self.params.task.lower() == 'chestxray':
             from attack_models.autoencoders import ChestXRayConditionalAutoencoder
-            from attack_models.unet import UNet
+            from attack_models.unet import UNet, ConditionalUNet
 
             input_dim = self.params.input_shape[1]
             n_classes = self.params.num_classes
             
             # atkmodel = ConditionalAutoencoder(n_classes, input_dim, pattern_tensor).to(self.params.device)
             # atkmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
-            atkmodel = ChestXRayConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
-            # atkmodel = UNet(n_classes, input_dim, 3).to(self.params.device)
+            # atkmodel = ChestXRayConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            # atkmodel = ConditionalUNet(n_classes, input_dim, 3).to(self.params.device)
+            atkmodel = ConditionalUNet(n_classes, input_dim, 1).to(self.params.device)
             # atkmodel = Autoencoder().to(self.params.device)
             # atkmodel = UNet(3).to(self.params.device)
 
             # tgtmodel = ConditionalAutoencoder(n_classes, input_dim, pattern_tensor).to(self.params.device)
             # tgtmodel = ConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
-            tgtmodel = ChestXRayConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
-            # tgtmodel = UNet(n_classes, input_dim, 3).to(self.params.device)
+            # tgtmodel = ChestXRayConditionalAutoencoder(n_classes, input_dim).to(self.params.device)
+            # tgtmodel = ConditionalUNet(n_classes, input_dim, 3).to(self.params.device)
+            tgtmodel = ConditionalUNet(n_classes, input_dim, 1).to(self.params.device)
             # tgtmodel = Autoencoder().to(self.params.device)
             # tgtmodel = UNet(3).to(self.params.device)
             tgtmodel.load_state_dict(atkmodel.state_dict(), strict=True)
