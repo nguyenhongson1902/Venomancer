c9063ef283932f93c81018b91d028dfc57bbdcda
Son Nguyen <nguyenhongsonk62hust@gmail.com>
2024-02-09 23:00:53 +0700

    Refactor ResNet model and update ChestXRayTask

--- models/resnet_chestxray.py
+++ models/resnet_chestxray.py
@@ -64,6 +64,48 @@ class Bottleneck(nn.Module):
         return out
 
 
+# class ResNet(nn.Module):
+#     def __init__(self, block, num_blocks, num_classes=15):
+#         super(ResNet, self).__init__()
+#         self.in_planes = 64
+
+#         # self.conv0 = nn.Conv2d(1, 3, kernel_size=5, stride=1, padding=2, bias=False) # (1, 1024, 1024) -> (3, 1024, 1024)
+#         # self.bn0 = nn.BatchNorm2d(3)
+
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(64)
+#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+#         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+#         # self.linear = nn.Linear(25088, num_classes)
+#         # self.linear = nn.Linear(524288, num_classes)
+#         self.linear = nn.Linear(32768, num_classes) # (256, 256)
+#         # self.linear = nn.Linear(131072, num_classes) # (512, 512)
+#         # self.linear = nn.Linear(512*block.expansion, num_classes)
+
+#     def _make_layer(self, block, planes, num_blocks, stride):
+#         strides = [stride] + [1]*(num_blocks-1)
+#         layers = []
+#         for stride in strides:
+#             layers.append(block(self.in_planes, planes, stride))
+#             self.in_planes = planes * block.expansion
+#         return nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         # out = F.relu(self.bn0(self.conv0(x))) # (1, 1024, 1024) -> (3, 1024, 1024)
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         # out = F.relu(self.bn1(self.conv1(out))) # to be compatible with (1, 1024, 1024) -> (3, 1024, 1024)
+#         out = self.layer1(out)
+#         out = self.layer2(out)
+#         out = self.layer3(out)
+#         out = self.layer4(out)
+#         out = F.avg_pool2d(out, 4)
+#         out = out.view(out.size(0), -1)
+#         out = self.linear(out)
+#         return out
+
+
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=15):
         super(ResNet, self).__init__()
@@ -104,7 +146,7 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
-
+    
 
 def ResNet18():
     return ResNet(BasicBlock, [2,2,2,2])
