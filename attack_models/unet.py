cf052ac6efa69f7556f2509e626cda547d8cf0c7
Son Nguyen <nguyenhongsonk62hust@gmail.com>
2024-02-17 15:14:37 +0700

    Refactor ConditionalUNet and add new models for ChestXRay, FashionMNIST, and MNIST

--- attack_models/unet.py
+++ attack_models/unet.py
@@ -131,64 +131,200 @@ class ConditionalUNet(nn.Module):
         return out
     
 
-# class ConditionalUNet(nn.Module):
+class ChestXRayConditionalUNet(nn.Module): # ChestXRay
 
-#     def __init__(self, n_classes, input_dim, out_channel):
-#         super().__init__()
+    def __init__(self, n_classes, input_dim, out_channel):
+        super().__init__()
 
-#         self.label_emb = nn.Embedding(n_classes, n_classes)
-#         self.linear_c = nn.Linear(n_classes, 1 * input_dim * input_dim)
-#         self.n_classes = n_classes
-#         self.input_dim = input_dim
+        self.label_emb = nn.Embedding(n_classes, n_classes)
+        self.linear_c = nn.Linear(n_classes, 1 * input_dim * input_dim)
+        self.n_classes = n_classes
+        self.input_dim = input_dim
 
-#         self.dconv_down1 = double_conv(1 + 1, 64)
-#         self.dconv_down2 = double_conv(64, 128)
-#         self.dconv_down3 = double_conv(128, 256)
-#         self.dconv_down4 = double_conv(256, 512)
+        self.dconv_down1 = double_conv(1 + 1, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)
 
-#         self.maxpool = nn.AvgPool2d(2)
-#         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear',
-#                                     align_corners=True)
+        self.maxpool = nn.AvgPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear',
+                                    align_corners=True)
 
-#         self.dconv_up3 = double_conv(256 + 512, 256)
-#         self.dconv_up2 = double_conv(128 + 256, 128)
-#         self.dconv_up1 = double_conv(128 + 64, 64)
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
 
-#         self.conv_last = nn.Sequential(
-#             nn.Conv2d(64, out_channel, 1),
-#             nn.BatchNorm2d(out_channel),
-#         )
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(64, out_channel, 1),
+            nn.BatchNorm2d(out_channel),
+        )
 
-#     def forward(self, x, c):
-#         c = self.linear_c(self.label_emb(c)).view(-1, 1, self.input_dim, self.input_dim)
-#         x = torch.cat([x, c], dim=1)
+    def forward(self, x, c):
+        c = self.linear_c(self.label_emb(c)).view(-1, 1, self.input_dim, self.input_dim)
+        x = torch.cat([x, c], dim=1)
         
-#         conv1 = self.dconv_down1(x)
-#         x = self.maxpool(conv1)
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
 
-#         conv2 = self.dconv_down2(x)
-#         x = self.maxpool(conv2)
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
 
-#         conv3 = self.dconv_down3(x)
-#         x = self.maxpool(conv3)
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)
 
-#         x = self.dconv_down4(x)
+        x = self.dconv_down4(x)
 
-#         x = self.upsample(x)
-#         x = torch.cat([x, conv3], dim=1)
+        x = self.upsample(x)
+        x = torch.cat([x, conv3], dim=1)
 
-#         x = self.dconv_up3(x)
-#         x = self.upsample(x)
-#         x = torch.cat([x, conv2], dim=1)
+        x = self.dconv_up3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
 
-#         x = self.dconv_up2(x)
-#         x = self.upsample(x)
-#         x = torch.cat([x, conv1], dim=1)
+        x = self.dconv_up2(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv1], dim=1)
 
-#         x = self.dconv_up1(x)
+        x = self.dconv_up1(x)
 
-#         out = self.conv_last(x)
+        out = self.conv_last(x)
 
-#         out = F.tanh(out)
+        out = F.tanh(out)
 
-#         return out
+        return out
+    
+
+class FashionMNISTConditionalUNet(nn.Module): # FashionMNIST
+
+    def __init__(self, n_classes, input_dim, out_channel):
+        super().__init__()
+
+        self.label_emb = nn.Embedding(n_classes, n_classes)
+        self.linear_c = nn.Linear(n_classes, 1 * input_dim * input_dim)
+        self.n_classes = n_classes
+        self.input_dim = input_dim
+
+        # self.conv0 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv0 = nn.Conv2d(2, 2, kernel_size=3, stride=1, padding=3, bias=False) # (2, 28, 28) -> (2, 32, 32)
+
+        self.dconv_down1 = double_conv(1 + 1, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)
+
+        self.maxpool = nn.AvgPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear',
+                                    align_corners=True)
+
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(64, out_channel, kernel_size=5, padding=0, stride=1),
+            nn.BatchNorm2d(out_channel),
+        )
+
+    def forward(self, x, c):
+        c = self.linear_c(self.label_emb(c)).view(-1, 1, self.input_dim, self.input_dim)
+        x = torch.cat([x, c], dim=1)
+        
+        x = self.conv0(x) # (2, 32, 32)
+
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)
+
+        x = self.dconv_down4(x)
+
+        x = self.upsample(x)
+        x = torch.cat([x, conv3], dim=1)
+
+        x = self.dconv_up3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv1], dim=1)
+
+        x = self.dconv_up1(x)
+
+        out = self.conv_last(x)
+
+        out = F.tanh(out)
+
+        return out
+    
+
+class MNISTConditionalUNet(nn.Module): # FashionMNIST
+
+    def __init__(self, n_classes, input_dim, out_channel):
+        super().__init__()
+
+        self.label_emb = nn.Embedding(n_classes, n_classes)
+        self.linear_c = nn.Linear(n_classes, 1 * input_dim * input_dim)
+        self.n_classes = n_classes
+        self.input_dim = input_dim
+
+        # self.conv0 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv0 = nn.Conv2d(2, 2, kernel_size=3, stride=1, padding=3, bias=False) # (2, 28, 28) -> (2, 32, 32)
+
+        self.dconv_down1 = double_conv(1 + 1, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)
+
+        self.maxpool = nn.AvgPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear',
+                                    align_corners=True)
+
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(64, out_channel, kernel_size=5, padding=0, stride=1),
+            nn.BatchNorm2d(out_channel),
+        )
+
+    def forward(self, x, c):
+        c = self.linear_c(self.label_emb(c)).view(-1, 1, self.input_dim, self.input_dim)
+        x = torch.cat([x, c], dim=1)
+        
+        x = self.conv0(x) # (2, 32, 32)
+
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)
+
+        x = self.dconv_down4(x)
+
+        x = self.upsample(x)
+        x = torch.cat([x, conv3], dim=1)
+
+        x = self.dconv_up3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv1], dim=1)
+
+        x = self.dconv_up1(x)
+
+        out = self.conv_last(x)
+
+        out = F.tanh(out)
+
+        return out
