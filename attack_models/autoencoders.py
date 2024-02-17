069133bf2ce10ef39c853fe7035dcf363da003b4
Son Nguyen <nguyenhongsonk62hust@gmail.com>
2024-02-17 15:14:27 +0700

    Add FashionMNISTConditionalAutoencoder class to autoencoders.py

--- attack_models/autoencoders.py
+++ attack_models/autoencoders.py
@@ -106,6 +106,44 @@ class MNISTConditionalAutoencoder(nn.Module):
         x = self.decoder(x)
         return x    
     
+class FashionMNISTConditionalAutoencoder(nn.Module):
+    def __init__(self, n_classes=10, input_dim=28):
+        super().__init__()
+        self.label_emb = nn.Embedding(n_classes, n_classes)
+        self.linear_c = nn.Linear(n_classes, 1 * input_dim * input_dim)
+        self.n_classes = n_classes
+        self.input_dim = input_dim
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1+1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+            nn.Conv2d(16, 64, 3, stride=2, padding=1),  # b, 8, 3, 3
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(64, 128, 3, stride=2),  # b, 16, 5, 5
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.BatchNorm2d(1),
+            nn.Tanh()
+        )
+
+    def forward(self, x, c):
+        c = self.linear_c(self.label_emb(c)).view(-1, 1, self.input_dim, self.input_dim)
+        x = torch.cat([x, c], dim=1)
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x   
+    
+    
 class ConditionalAutoencoder(nn.Module):
     def __init__(self, n_classes, input_dim):
         super().__init__()
